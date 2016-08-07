2b7ad8845de1196aabf8ca8bae0151357373777d
Alex DeCapri <alexdecapri@gmail.com>
2016-08-07 15:37:48 -0600

    Wiring up reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,12 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
   // console.log('Action received', action);
+  switch (action.type) {
+  case FETCH_WEATHER:
+    // return state.concat([action.payload.data]); // Put in array because multiple cities will be in this list
+    return [ action.payload.data, ...state ]; // make new array, put action.payload.data inside of it, insert that new record at top of array
+  }
+
   return state;
 }
