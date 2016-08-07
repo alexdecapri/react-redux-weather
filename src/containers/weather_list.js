d8a2379f4bc0d93d547072961c94c6f6087b2420
Alex DeCapri <alexdecapri@gmail.com>
2016-08-07 17:12:17 -0600

    Google Maps error, last step until finish

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -9,11 +10,16 @@ class WeatherList extends Component {
     // console.log(temps);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+    // const lon = cityData.city.coord.lon;
+    // const lat = cityData.city.coord.lat;
+    console.log('lon', lon);
+    console.log('lat', lat);
 
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lat={lat} lng={lon} /></td>
         <td><Chart data={temps} color="orange" units="K" /></td>
         <td><Chart data={pressures} color="green" units="hPa" /></td>
         <td><Chart data={humidities} color="black" units="%" /></td>
