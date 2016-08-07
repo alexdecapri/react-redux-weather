f1390b53800d1fb34cf0cff61f43662a3748497e
Alex DeCapri <alexdecapri@gmail.com>
2016-08-07 15:26:30 -0600

    Add weather-reducer, get redux-promise working

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,9 +8,14 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url); // Returns promise assigned to variable request
+  // redux-promise makes this clean^^ :)
+
+  // console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
     payload: request
+    // if this is a promise, redux-promise stops the action entirely
+    // once request finishes, then dispatches a new action of the same type but with a payload of the resolved request
   };
 }
