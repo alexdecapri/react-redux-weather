cac34e1468d5137357b014aca460957453848e3f
Alex DeCapri <alexdecapri@gmail.com>
2016-09-02 14:56:21 -0600

    google maps api needs http not https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'c8969cdf2826c2e9a345e56f969520c2';
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
